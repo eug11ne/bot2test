0f3d04eac389cb3ada11cc7223eaf36b76df8dfc
eug11ne <114510061+eug11ne@users.noreply.github.com>
2022-12-08 12:02:41 +0300

    Update bot

--- main.py
+++ main.py
@@ -1,110 +1,248 @@
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
-from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, CallbackContext
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, CallbackContext, ConversationHandler, MessageHandler, Filters
+
+
+MAIN_MENU, NEXT_STEPS, SALON, SERVICE, MASTER, ENTER_DATE, TIME_SLOTS, REGISTER, ENTER_CONTACT_INFO, ORDERS = range(10)
 
 
 def start(update: Update, context: CallbackContext) -> None:
     """Sends a message with three inline buttons attached."""
+    kbd = ['Записаться', 'Открыть личный кабинет']
+    reply_markup = create_keyboard(kbd)
 
-    update.message.reply_text('Please choose:',
-                              reply_markup = keyboard_main_menu())
+    update.message.reply_text('Что вы хотели бы сделать:',
+                              reply_markup=reply_markup)
+    return MAIN_MENU
+
+def cancel(update: Update, context: CallbackContext) -> None:
+    return ConversationHandler.END
 
 
 def main_menu(update: Update, context: CallbackContext) -> None:
     """ Displays the main menu keyboard when called. """
 
+
     query = update.callback_query
     query.answer()
-    query.edit_message_text(text         = 'Please choose:',
-                            reply_markup = keyboard_main_menu())
+    kbd = ['Записаться', 'Открыть личный кабинет']
+    reply_markup = create_keyboard(kbd)
+    query.edit_message_text(text         = 'Что вы хотели бы сделать',
+                            reply_markup = reply_markup)
+    return MAIN_MENU
 
 
-def keyboard_main_menu():
-    """ Creates the main menu keyboard """
+def main_submenu(update: Update, context: CallbackContext) -> None:
+    context.user_data.update({'choose_service_first': False, 'choose_master_first': False})
 
-    keyboard = [
-        [InlineKeyboardButton("Option 1", callback_data='1'),
-         InlineKeyboardButton("Option 2", callback_data='2'),
-         InlineKeyboardButton("Option 3", callback_data='VVV')]
-    ]
+    next_steps = ['Выбрать салон', 'Выбрать услугу', 'Выбрать мастера']
 
+    reply_markup = create_keyboard(next_steps)
+
+    query = update.callback_query
+    query.answer()
+
+    query.edit_message_text(text="Вы хотели бы:", reply_markup=reply_markup)
+    return NEXT_STEPS
+
+
+
+def client_area(update: Update, context: CallbackContext) -> None:
+    orders = ['122234', '344224', '333223', '12984']
+    reply_markup = create_keyboard(orders)
+    query = update.callback_query
+    query.answer()
+    query.edit_message_text(text="Вы можете повторить любой из предыдущих заказов:", reply_markup=reply_markup)
+    return ORDERS
+
+def choose_order(update: Update, context: CallbackContext) -> None:
+    reorder = ['Заказать снова']
+    reply_markup = create_keyboard(reorder)
+    query = update.callback_query
+    query.answer()
+    query.edit_message_text(text="Вы заказывали массаж в Салоне на Павелецкой у мастера Ивановой:", reply_markup=reply_markup)
+
+
+
+def choose_salon(update: Update, context: CallbackContext) -> None:
+
+    salons = ['Салон на Павелецкой',
+              'Салон на Пресне',
+              'Салон в Сити',
+              'Салон на Кутузовском'
+              ]
+
+    reply_markup = create_keyboard(salons)
+
+    query = update.callback_query
+    query.answer()
+    query.edit_message_text(text="Выберите подходящий салон:", reply_markup=reply_markup)
+
+    if context.user_data['choose_master_first']:
+        return ENTER_DATE
+    elif context.user_data['choose_service_first']:
+        return MASTER
+    else:
+        return SERVICE
+
+def choose_service(update: Update, context: CallbackContext) -> None:
+
+    services = ['Супер-детокс обертывание',
+    'Томатное обертывание',
+    'Парафинотерапия',
+    'Автозагар',
+    'Водорослевый пилинг',
+    'Парафиновые ванны',
+    'Массажная программа']
+
+
+    reply_markup = create_keyboard(services)
+    print(context.user_data)
+
+    query = update.callback_query
+    query.answer()
+    query.edit_message_text(text=f"Выберите одну из следующих услуг:", reply_markup=reply_markup)
+
+    if context.user_data['choose_master_first']:
+        return SALON
+    return MASTER
+
+def choose_master(update: Update, context: CallbackContext) -> None:
+
+    masters = ['Иванова',
+    'Сергеева',
+    'Цветкова',
+    'Капустина']
+
+
+    reply_markup = create_keyboard(masters)
+    print(context.user_data)
+
+    query = update.callback_query
+    query.answer()
+    query.edit_message_text(text="Выбранную услугу может оказать один из следующих мастеров:", reply_markup=reply_markup)
+    return ENTER_DATE
+
+def choose_date(update: Update, context: CallbackContext) -> None:
+
+    query = update.callback_query
+    query.answer()
+    query.edit_message_text(text="Выберите дату:")
+
+
+def enter_date(update, context: CallbackContext) -> None:
+    date = update.message.text
+    update.message.reply_text(date)
+    masters = ['10:00 - 11:00',
+               '11:00 - 12:00',
+               '11:00 - 13:00',
+               '13:00 - 14:00',
+               '14:00 - 15:00',
+               ]
+
+    reply_markup = create_keyboard(masters)
+    update.message.reply_text("Bыберите время:", reply_markup=reply_markup)
+    return REGISTER
+
+
+def register_client(update: Update, context: CallbackContext) -> None:
+
+    registration = ['Я согласен',
+                    'Я не согласен'
+                    ]
+
+    reply_markup = create_keyboard(registration)
+    query = update.callback_query
+    query.answer()
+    query.edit_message_text(text="Нам нужно ваше согласие на обработку персональных данных:", reply_markup=reply_markup)
+    return ENTER_CONTACT_INFO
+
+def enter_contact_info(update, context: CallbackContext) -> None:
+    phone = update.message.text
+    update.message.reply_text(phone)
+    reply_markup = create_keyboard(['Оплатить'])
+    update.message.reply_text("Осталось оплатить заказ:", reply_markup=reply_markup)
+
+
+def choose_contact_info(update, context: CallbackContext) -> None:
+    query = update.callback_query
+    query.answer()
+    query.edit_message_text(text="Введите ваш номер телефона:")
+
+
+def choose_service_first(update: Update, context: CallbackContext) -> None:
+
+    context.user_data.update({'choose_service_first': True})
+
+    services = [
+        'Супер-детокс обертывание',
+        'Томатное обертывание',
+        'Парафинотерапия',
+        'Автозагар',
+        'Водорослевый пилинг',
+        'Парафиновые ванны',
+        'Массажная программа'
+                ]
+
+
+    reply_markup = create_keyboard(services)
+    print(context.user_data)
+
+    query = update.callback_query
+    query.answer()
+    query.edit_message_text(text="Выберите одну из следующих услуг, чтобы найти салоны, в которых она предоставляется:", reply_markup=reply_markup)
+    return SALON
+
+
+def choose_master_first(update: Update, context: CallbackContext) -> None:
+    context.user_data.update({'choose_master_first': True})
+    masters = [
+        'Иванова',
+        'Сергеева',
+        'Цветкова',
+        'Капустина'
+        ]
+
+
+    reply_markup = create_keyboard(masters)
+    print(context.user_data)
+
+    query = update.callback_query
+    query.answer()
+    query.edit_message_text(text="Выберите мастера, к которому вы хотели бы обратиться:", reply_markup=reply_markup)
+    return SERVICE
+
+def create_keyboard(button_names):
+    keyboard = [[InlineKeyboardButton(kb, callback_data=f'{button_names.index(kb)}')] for kb in button_names]
     return InlineKeyboardMarkup(keyboard)
 
-
-def confirm(update: Update, context: CallbackContext) -> None:
-    """Parses the CallbackQuery and updates the message text."""
-
-    query = update.callback_query
-    query.answer()
-
-    keyboard = [
-        [InlineKeyboardButton("Yes", callback_data=f'YES{query.data}'),
-         InlineKeyboardButton("No",  callback_data='main'),],
-    ]
-
-    reply_markup = InlineKeyboardMarkup(keyboard)
-
-    query.edit_message_text(text=f"Selected option {query.data}.\n"
-                                 f"Are you sure ? ",
-                           reply_markup=reply_markup)
-    #query.message.reply_text(text=f"Selected option {query.data}.\n"
-    #                             f"Are you sure ? ",
-    #                        reply_markup=reply_markup)
-
-
-
-
-def do_action_1(update: Update, context: CallbackContext) -> None:
-
-    keyboard = [[InlineKeyboardButton("Main menu", callback_data='main')]]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-
-    query = update.callback_query
-    query.answer()
-    query.edit_message_text(text=f"Selected option {query.data}\n"
-                                 f"Executed action 1.",
-                            reply_markup=reply_markup)
-
-
-def do_action_2(update: Update, context: CallbackContext) -> None:
-
-    keyboard = [[InlineKeyboardButton("Main menu", callback_data='main')]]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-
-    query = update.callback_query
-    query.answer()
-    query.edit_message_text(text=f"Selected option {query.data}\n"
-                                 f"Executed action 2.",
-                            reply_markup=reply_markup)
-
-def do_action_3(update: Update, context: CallbackContext) -> None:
-    query = update.callback_query
-    query.answer()
-    clubs = ['Салон в центре', 'Салон слева', 'Салон справа']
-    keyboard = [[InlineKeyboardButton(kb, callback_data=kb) for kb in clubs]]
-    #for i in range(5):
-    query.message.reply_text(text=f"Hello world {query.data}:", reply_markup=InlineKeyboardMarkup(keyboard))
-
-def do456(update: Update, context: CallbackContext) -> None:
-    query = update.callback_query
-    query.answer()
-
-    query.message.reply_text(text=f"Hello world {query.data}:")
 
 
 
 def main() -> None:
     """Run the bot."""
     # Create the Updater and pass it your bot's token.
+
     updater = Updater("5939603614:AAHQRADVt5SVleCzifGK3nUH1tyJeNfZ104")
 
-    updater.dispatcher.add_handler(CommandHandler('start', start))
-    updater.dispatcher.add_handler(CallbackQueryHandler(main_menu, pattern='main'))
-    updater.dispatcher.add_handler(CallbackQueryHandler(do_action_3, pattern='VVV'))
-    updater.dispatcher.add_handler(CallbackQueryHandler(confirm, pattern='^(|1|2)$'))
-    updater.dispatcher.add_handler(CallbackQueryHandler(do_action_1, pattern='YES1'))
-    updater.dispatcher.add_handler(CallbackQueryHandler(do_action_2, pattern='YES2'))
-    updater.dispatcher.add_handler(CallbackQueryHandler(do456, pattern='(|Салон в центре|Салон слева|Салон справа)'))
+    dispatcher = updater.dispatcher
 
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+        states={
+            MAIN_MENU: [CallbackQueryHandler(main_submenu, pattern='0'), CallbackQueryHandler(client_area, pattern='1')],
+            NEXT_STEPS: [CallbackQueryHandler(choose_salon, pattern='0'), CallbackQueryHandler(choose_service_first, pattern='1'), CallbackQueryHandler(choose_master_first, pattern='2')],
+            SALON: [CallbackQueryHandler(choose_salon, pattern='\d+')],
+            SERVICE: [CallbackQueryHandler(choose_service, pattern='\d+')],
+            MASTER: [CallbackQueryHandler(choose_master, pattern='\d+')],
+            ENTER_DATE: [CallbackQueryHandler(choose_date, pattern='\d+'), MessageHandler(Filters.text, enter_date)],
+            REGISTER: [CallbackQueryHandler(register_client, pattern='\d+')],
+            ENTER_CONTACT_INFO: [CallbackQueryHandler(choose_contact_info, pattern='\d+'), MessageHandler(Filters.text, enter_contact_info)],
+            ORDERS: [CallbackQueryHandler(choose_order, pattern='\d+')],
+        },
+        fallbacks=[CommandHandler('cancel', cancel)],
+    )
+
+    dispatcher.add_handler(conv_handler)
 
 
     # Start the Bot
