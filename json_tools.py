9ea8f3a35623af9b9da8165b201b95c1d2cd73d9
eug11ne <114510061+eug11ne@users.noreply.github.com>
2022-12-09 10:21:09 +0300

    Add json support

---
+++ json_tools.py
@@ -0,0 +1,38 @@
+import json
+
+def load_json():
+    with open('config.json') as f:
+        json_data = json.load(f)
+    return json_data
+
+def get_master_names(json_data, salon):
+    masters = []
+    for master in json_data['MASTERS']:
+        if salon in json_data['MASTERS'][master]['salons']:
+            masters.append(json_data['MASTERS'][master]['name'])
+    return masters
+
+def get_all_master_names(json_data):
+    masters = []
+    for master in json_data['MASTERS']:
+        masters.append(json_data['MASTERS'][master]['name'])
+    return masters
+
+def get_salon_names(json_data):
+    salons = []
+    for salon in json_data['SALONS']:
+        salons.append(json_data['SALONS'][salon]['name'])
+    return salons
+
+def get_service_names(json_data):
+    services = []
+    for service in json_data['SERVICES']:
+        services.append(json_data['SERVICES'][service]['name'])
+    return services
+
+
+
+
+
+
+
